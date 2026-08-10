6a7e806a3a53e93336da85022b0e49fe0dcd1eae
academiahye <hyeacademydigital@gmail.com>
2026-08-10 17:55:17 -0500

    elminiar contenido de confi __init__.py completo

--- config/settings/__init__.py
+++
@@ -1,9 +0,0 @@
-#import os
-
-# Lee la variable de entorno o carga local por defecto
-#environment = os.getenv('DJANGO_ENV', 'local')
-
-#if environment == 'production':
-#    from .production import *
-#else:
-#    from .local import *
