5c0781e9c7cacc7b00ef42f667de1255bc5f18f8
academiahye <hyeacademydigital@gmail.com>
2026-08-10 17:14:33 -0500

    config con decouple archivo production.py con dotenv

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,49 +1,22 @@
-#import os
-#from .base import *
-
-#DEBUG = False
-
-# Hosts permitidos
-#ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='127.0.0.1,localhost').split(',')
-
-# Seguridad HTTP / Cabeceras
-#SECURE_BROWSER_XSS_FILTER = True
-#SECURE_CONTENT_TYPE_NOSNIFF = True
-#X_FRAME_OPTIONS = 'DENY'
-
-# Manejo de SSL / HTTPS (si no usas certificado SSL en el servidor, déjalos en False en el .env)
-#CSRF_COOKIE_SECURE = os.getenv('CSRF_COOKIE_SECURE', 'False') == 'True'
-#SESSION_COOKIE_SECURE = os.getenv('SESSION_COOKIE_SECURE', 'False') == 'True'
-#SECURE_SSL_REDIRECT = os.getenv('SECURE_SSL_REDIRECT', 'False') == 'True'
-
-# Manejo de Estáticos y Media en Producción
-#STATIC_URL = '/static/'
-#STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-#MEDIA_URL = '/media/'
-#MEDIA_ROOT = BASE_DIR / 'media'
-
-# Orígenes confiables para evitar bloqueos por CSRF en peticiones POST (importación Excel, etc.)
-#CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', default='http://127.0.0.1').split(',')
 import os
-from decouple import config
 from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost').split(',')
+# Hosts permitidos (lee del .env)
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='127.0.0.1,localhost').split(',')
 
 # Seguridad HTTP / Cabeceras
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 X_FRAME_OPTIONS = 'DENY'
 
-# SSL / Cookies (desactivado para HTTP interno detrás del proxy)
+# SSL / Cookies (desactivado porque el proxy de Virtualmin ya maneja HTTPS)
 CSRF_COOKIE_SECURE = False
 SESSION_COOKIE_SECURE = False
 SECURE_SSL_REDIRECT = False
 
-# Indicar a Django que el proxy de Virtualmin gestiona SSL/HTTPS
+# Indicar a Django que el proxy maneja SSL/HTTPS (necesario para que las URLs generadas sean https)
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Manejo de Estáticos y Media en Producción
@@ -53,5 +26,5 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-# Orígenes confiables para CSRF
-CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='https://inventario.iestparib.edu.pe').split(',')
+# Orígenes confiables para CSRF (lee del .env)
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', default='https://inventario.iestparib.edu.pe,http://inventario.iestparib.edu.pe').split(',')
