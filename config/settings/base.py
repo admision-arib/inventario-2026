80606fad965bc606a396b195a1d44e39a9c92679
academiahye <hyeacademydigital@gmail.com>
2026-08-12 09:23:55 -0500

    agregar variables BASE_URL en base.py para qr 2

--- config/settings/base.py
+++ config/settings/base.py
@@ -10,7 +10,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost').split(',')
 
-BASE_URL = os.getenv('BASE_URL', 'http://127.0.0.1:8000')
+BASE_URL = config('BASE_URL', default='http://127.0.0.1:8000')
 
 # APPLICACIONES
 INSTALLED_APPS = [
